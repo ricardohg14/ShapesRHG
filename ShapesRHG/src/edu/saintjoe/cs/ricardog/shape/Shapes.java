e107e159049a8bb1f053f15b8b5088c2fc1c91fc
ricardog <ricardog@localhost>
2016-04-15 14:31:19 -0400

    start off commit

---
+++ ShapesRHG/src/edu/saintjoe/cs/ricardog/shape/Shapes.java
@@ -0,0 +1,13 @@
+package edu.saintjoe.cs.ricardog.shape;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public void sayHello () {
+		System.out.println("Hello");
+	}
+
+}
---
+++ ShapesRHG/src/edu/saintjoe/cs/ricardog/shape/Square.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.ricardog.shape;
+
+public class Square extends Shapes {
+	
+	
+	public static void main(String[] args) {
+		Square mySquare = new Square();
+		
+		mySquare.sayHello();
+	}
+}
+
